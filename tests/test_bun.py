de1cab559c0f6be53ebe5d32cc2993e3a29de7df
Andrey Mozhaev <Mozhaev.andrey.v@yandex.com>
2025-02-01 21:59:33 +0300

    func: add test_bun.py and assert test_get_valid_name

---
+++ tests/test_bun.py
@@ -0,0 +1,7 @@
+from praktikum.bun import Bun
+
+class TestBun:
+
+    def test_get_valid_name(self):
+        bun = Bun('Краторная булка N-200i', 1255)
+        assert bun.get_name() == 'Краторная булка N-200i'
