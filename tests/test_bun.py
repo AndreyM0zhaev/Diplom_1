1494920adf0fb1932db8150e4e27c97f8c888860
Andrey Mozhaev <Mozhaev.andrey.v@yandex.com>
2025-02-01 22:02:08 +0300

    func: add assert test_get_valid_price

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -5,3 +5,7 @@ class TestBun:
     def test_get_valid_name(self):
         bun = Bun('Краторная булка N-200i', 1255)
         assert bun.get_name() == 'Краторная булка N-200i'
+
+    def test_get_valid_price(self):
+        bun = Bun('Краторная булка N-200i', 1255)
+        assert bun.get_price() == 1255
