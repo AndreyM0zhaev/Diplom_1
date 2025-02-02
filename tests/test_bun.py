b00f741d79d4034901a6df43ecd88c63730fa44d
Andrey Mozhaev <Mozhaev.andrey.v@yandex.com>
2025-02-02 23:58:55 +0300

    func: add test_ingredient asserts

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -3,9 +3,9 @@ from praktikum.bun import Bun
 class TestBun:
 
     def test_get_bun_name(self):
-        bun = Bun('Краторная булка N-200i', 1255)
-        assert bun.get_name() == 'Краторная булка N-200i'
+        bun = Bun('Флюоресцентная булка R2-D3', 988)
+        assert bun.get_name() == 'Флюоресцентная булка R2-D3', "Неверное название"
 
     def test_get_bun_price(self):
         bun = Bun('Краторная булка N-200i', 1255)
-        assert bun.get_price() == 1255
+        assert bun.get_price() == 1255, "Неверная цена"
--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -1,5 +1,8 @@
 from praktikum.ingredient import Ingredient
 from praktikum.ingredient_types import IngredientTypes
+import pytest
+
+
 
 class TestIngredient:
 
@@ -7,8 +10,20 @@ class TestIngredient:
         ingredient = Ingredient(IngredientTypes.INGREDIENT_TYPE_SAUCE, "Соус Spicy-X", 90)
         assert ingredient.get_price() == 90, "Неверная цена ингредиента"
 
+
     def test_get_ingredient_name(self):
-        ingredient = Ingredient(IngredientTypes.INGREDIENT_TYPE_SAUCE, "Соус Spicy-X", 90)
-        assert ingredient.get_name() == "Соус Spicy-X", "Неверное название ингредиента"
+        ingredient = Ingredient(IngredientTypes.INGREDIENT_TYPE_FILLING, "Сыр с астероидной плесенью", 4142)
+        assert ingredient.get_name() == "Сыр с астероидной плесенью", "Неверное название ингредиента"
 
 
+    @pytest.mark.parametrize(
+        'type, name, price',
+        [
+            ['Соус фирменный Space Sauce', 80, IngredientTypes.INGREDIENT_TYPE_SAUCE],
+            ['Филе Люминесцентного тетраодонтимформа', 988, IngredientTypes.INGREDIENT_TYPE_FILLING]
+        ]
+    )
+
+    def test_get_ingredient_type(self, type, name, price):
+        ingredient = Ingredient(type, name, price)
+        assert ingredient.get_type() == type
