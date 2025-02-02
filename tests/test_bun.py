6bc4d411d1c4cf7eaff3b5518c23186fe95877ae
Andrey Mozhaev <Mozhaev.andrey.v@yandex.com>
2025-02-03 00:04:21 +0300

    func: add test_burger.py

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,10 +1,13 @@
 from praktikum.bun import Bun
 
+
+
 class TestBun:
 
     def test_get_bun_name(self):
         bun = Bun('Флюоресцентная булка R2-D3', 988)
         assert bun.get_name() == 'Флюоресцентная булка R2-D3', "Неверное название"
+
 
     def test_get_bun_price(self):
         bun = Bun('Краторная булка N-200i', 1255)
---
+++ tests/test_burger.py
@@ -0,0 +1,9 @@
+
+
+
+
+class TestBurger:
+
+    def set_buns(self):
+        burger = Burger()
+        bun = Bun()
