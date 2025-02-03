52a529d129fa9aeedbd0f722effb03cb9c2eb66c
Andrey Mozhaev <Mozhaev.andrey.v@yandex.com>
2025-02-03 23:12:41 +0300

    func: add tests: set_buns, add_ingredient

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,9 +1,22 @@
+from unittest.mock import Mock
 
+from praktikum.burger import Burger
+from praktikum.bun import Bun
+from praktikum.ingredient_types import IngredientTypes
 
 
 
 class TestBurger:
 
-    def set_buns(self):
+    def test_set_buns(self):
         burger = Burger()
-        bun = Bun()
+        bun = Bun('Флюоресцентная булка R2-D3', 988)
+        burger.set_buns(bun)
+        assert burger.bun == bun, "Булочка не была установлена"
+
+    def test_add_ingredient(self):
+        burger = Burger()
+        mock_ingredient = Mock('sauce', 'жгучий перчик', 88)
+        burger.add_ingredient(mock_ingredient)
+        assert mock_ingredient in burger.ingredients, "Ингредиент не был добавлен"
+
