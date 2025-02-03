6c2d2bc13693095ac591cb24700f0577a36f427b
Andrey Mozhaev <Mozhaev.andrey.v@yandex.com>
2025-02-03 23:18:46 +0300

    func: add tests: remove_ingredient

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -20,3 +20,12 @@ class TestBurger:
         burger.add_ingredient(mock_ingredient)
         assert mock_ingredient in burger.ingredients, "Ингредиент не был добавлен"
 
+    def test_remove_ingredient(self):
+        burger = Burger()
+        mock_ingredient0 = Mock('sauce', 'жгучий перчик', 88)
+        mock_ingredient1 = Mock('filling', 'эктоплазма', 111)
+        burger.add_ingredient(mock_ingredient0)
+        burger.add_ingredient(mock_ingredient1)
+        burger.remove_ingredient(0)
+        assert mock_ingredient0 not in burger.ingredients, "Ингредиент не был удален"
+        assert mock_ingredient1 in burger.ingredients, "Неправильный ингредиент был удален"
